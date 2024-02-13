280b3ec8108b52e95deca253ac18f21b846d572a
Michael112007 <117554393+Michael112007@users.noreply.github.com>
2024-02-12 20:43:10 -0400

    Agregar archivos de proyecto.

---
+++ leer_contraseña/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("leer cantidad de numeros");
+Console.WriteLine();
+
+
+{
+    string contraseña = "micha"; 
+    int intentos = 3;
+
+    while (intentos > 0)
+    {
+        Console.Write("Ingrese la contraseña: ");
+        string entrada = Console.ReadLine();
+
+        if (entrada == contraseña)
+        {
+            Console.WriteLine("¡Contraseña correcta!");
+            return;
+        }
+        else
+        {
+            intentos--;
+            Console.WriteLine("Contraseña incorrecta. Te quedan " + intentos + " intentos.");
+        }
+    }
+
+    Console.WriteLine("Has agotado el número de intentos. El programa terminará.");
+}
+
+
